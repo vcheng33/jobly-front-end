b5936ae923f1323e35dbcbbbe9692dd0221dc4f4
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-21 09:25:31 -0700

    added pseudocode for App

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ import "bootstrap/dist/css/bootstrap.css";
  *  - None
  * 
  *  State:
- *  - None
+ *  - token
+ *  - currentUser
  * 
  *  App -> { Navigation, Routes }
  */
 function App() {
+  // Create a function to login a user
+
+  // Create a function to signup a user
+
+  // Create a function logout a user
+
+  // Pass login and signup into Routes
+  // Pass logout into Navigation
+
+  // create an effect that is triggered when token is updated
+  // it will be an async function that updates currentUser
+
   return (
     <div className="App">
       <BrowserRouter>
