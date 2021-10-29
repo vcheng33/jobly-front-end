62d3f6c8164cc1111c126f9e353bbb89e0a23f1c
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-29 15:34:13 -0700

    updated favicon

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 import UserContext from "./UserContext";
+import { NavbarToggler, Collapse } from "reactstrap";
 
 /** NavBar for Jobly App
  * 
@@ -16,7 +17,7 @@ import UserContext from "./UserContext";
  */
 
 function Navigation({ handleLogout }) {
-  const { currentUser }= useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
 
   return (
     <nav className="Navigation navbar navbar-expand-sm">
@@ -24,41 +25,47 @@ function Navigation({ handleLogout }) {
         <NavLink exact to="/" id="navbar-Jobly" className="navbar-brand text-white ms-2">
           Jobly
         </NavLink>
-        
-        {currentUser && <ul className="navbar-nav ms-auto">
-          <li className="nav-item me-3" >
-            <NavLink exact to="/companies" className="text-decoration-none">
-              Companies
-            </NavLink>
-          </li>
-          <li className="nav-item me-3" >
-            <NavLink exact to="/jobs" className="text-decoration-none">
-              Jobs
-            </NavLink>
-          </li>
-          <li className="nav-item me-3" >
-            <NavLink exact to="/profile" className="text-decoration-none">
-              Profile
-            </NavLink>
-          </li>
-          <li className="nav-item" >
-            <NavLink exact to="/" className="text-decoration-none" onClick={handleLogout}>
-              Logout
-            </NavLink>
-          </li>
-        </ul>}
-        {!currentUser && <ul className="navbar-nav ms-auto">
-          <li className="nav-item me-4" >
-            <NavLink exact to="/login" className="text-decoration-none">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item text-nowrap" >
-            <NavLink exact to="/signup" className="text-decoration-none">
-              Sign Up
-            </NavLink>
-          </li>
-        </ul>}
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          {currentUser &&
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item me-3 active" >
+                <NavLink exact to="/companies" className="text-decoration-none">
+                  Companies
+                </NavLink>
+              </li>
+              <li className="nav-item me-3" >
+                <NavLink exact to="/jobs" className="text-decoration-none">
+                  Jobs
+                </NavLink>
+              </li>
+              <li className="nav-item me-3" >
+                <NavLink exact to="/profile" className="text-decoration-none">
+                  Profile
+                </NavLink>
+              </li>
+              <li className="nav-item" >
+                <NavLink exact to="/" className="text-decoration-none" onClick={handleLogout}>
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          }
+          {!currentUser && <ul className="navbar-nav ms-auto">
+            <li className="nav-item me-4" >
+              <NavLink exact to="/login" className="text-decoration-none">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item text-nowrap" >
+              <NavLink exact to="/signup" className="text-decoration-none">
+                Sign Up
+              </NavLink>
+            </li>
+          </ul>}
+        </div>
       </div>
     </nav>
   );
